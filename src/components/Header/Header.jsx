2e3e1d3ab76d1e510046ab3f0942b7f5a021537e
chudini-creator <igorczew52@gmail.com>
2026-04-04 18:19:17 +0200

    Poprawki

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import './Header.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+
 function Header() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
     return (
         <header className='Header'>
             <div className='HeaderContainer'>
@@ -12,7 +16,35 @@ function Header() {
 
                 <nav className='navs'>
                     <ul>
-                        <li><NavLink to="/o-fundacji">O fundacji</NavLink></li>
+                        <li 
+                            className='dropdown-wrapper'
+                            onMouseEnter={() => setDropdownOpen(true)}
+                            onMouseLeave={() => setDropdownOpen(false)}
+                        >
+                            <NavLink to="/o-fundacji">O fundacji</NavLink>
+                            <div className={`dropdown-menu ${dropdownOpen ? 'dropdown-menu--open' : ''}`}>
+                            <NavLink 
+                                to="/o-fundacji#pomysl" 
+                                className="dropdown-item"
+                                onClick={() => setDropdownOpen(false)}
+                            >
+                                Skąd pomysł?
+                            </NavLink>
+                            <NavLink 
+                                to="/o-fundacji#nazwa" 
+                                className="dropdown-item"
+                                onClick={() => setDropdownOpen(false)}
+                            >
+                                Skąd nazwa?
+                            </NavLink>
+                            <NavLink to="/zespół" className="dropdown-item">
+                                Nasz zespół
+                            </NavLink>
+                            <NavLink to="/statut" className="dropdown-item">
+                                Statut
+                            </NavLink>
+                            </div>
+                        </li>
                         <li><NavLink to="/projekty">Projekty</NavLink></li>
                         <li><NavLink to="/kontakt">Kontakt</NavLink></li>
                     </ul>
